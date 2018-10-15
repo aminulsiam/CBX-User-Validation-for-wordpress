a7a255e589e17ef49866b84727843882507e58c1
aminulsiam <aminulhossain90@gmail.com>
2018-10-15 12:47:12 +0600

    Registration successfully done , primary release

--- classes/user.php
+++ classes/user.php
@@ -27,11 +27,14 @@ class User
     public function store($data)
     {
         // Get the value and validate here
+
         $first_name = $this->validate->validate($data['first_name']);
         $last_name  = $this->validate->validate($data['last_name']);
         $email      = $this->validate->validate($data['email']);
         $password   = md5($data['password']);
+
         // insert user data
+
         $query = "INSERT INTO users(first_name,last_name,email,password) VALUES 
         ('$first_name','$last_name','$email ','$password')";
         $store = $this->db->insert($query);
